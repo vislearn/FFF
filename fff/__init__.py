07d7c094f53c3b34ca9c884a455e2c05df2518ea
hummerichsander <sander.hummerich@gmx.de>
2025-01-23 08:53:25 +0100

    add embedding normalization to GraphAutoencoder

--- fff/__init__.py
+++ fff/__init__.py
@@ -15,8 +15,3 @@ except ImportError:
 
 
 import fff.loss as loss
-import fff.data as data
-import fff.model as model
-from .fff import FreeFormFlowHParams, FreeFormFlow
-from .fif import FreeFormInjectiveFlowHParams, FreeFormInjectiveFlow
-from .m_fff import ManifoldFreeFormFlowHParams, ManifoldFreeFormFlow

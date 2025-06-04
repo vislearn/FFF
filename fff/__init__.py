c9f5b037d7164c176e426dbf1f15bc3a20430a79
Armand <armand.r@live.de>
2025-06-04 14:25:16 +0200

    Revert commit "add embedding normalization to GraphAutoencoder"
    This reverts commit 07d7c094f53c3b34ca9c884a455e2c05df2518ea, which erroneously removes the FreeFormFlow classes from the module __init__.py file.

--- fff/__init__.py
+++ fff/__init__.py
@@ -15,3 +15,8 @@ except ImportError:
 
 
 import fff.loss as loss
+import fff.data as data
+import fff.model as model
+from .fff import FreeFormFlowHParams, FreeFormFlow
+from .fif import FreeFormInjectiveFlowHParams, FreeFormInjectiveFlow
+from .m_fff import ManifoldFreeFormFlowHParams, ManifoldFreeFormFlow
